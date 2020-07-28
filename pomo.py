a2adcf7c76ee8c78c737c0ce356e0ff7b6d74b03
DakshKalley <dakshk117@gmail.com>
2020-07-28 16:32:46 -0700

    Displays proper countdown

--- pomo.py
+++ pomo.py
@@ -1,14 +1,14 @@
 from timer import timer, notify
-Study = int(input("Study Time: "))
+Work = int(input("Work Time: "))
 Break = int(input("Break Time: "))
 Sess = int(input("# of Sessions: "))
 
 while Sess > 0:
-    timer(Study, "~~ Study ~~")
+    timer(Work, "~~ Work ~~")
     notify("Pomodoro", "Break Start")
     timer(Break, "~~ Break ~~")
     notify("Pomodoro", "Break Over")
     Sess -= 1
 
-print("Study Session Complete!")
-notify("Pomodoro", "Study Session Complete!")
+print("Pomodoro Sessions Complete!")
+notify("Pomodoro", "Work Sessions Complete!")
