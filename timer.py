201fcd026cfc3fbc60ae93c432cf82dbfff506cd
DakshKalley <dakshk117@gmail.com>
2020-07-28 16:25:53 -0700

    Displays proper countdown

--- timer.py
+++ timer.py
@@ -2,13 +2,22 @@ import datetime
 import time
 import os
 
+def convsec(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return ("%d:%02d" % (minutes, seconds))
+
+
 def timer(min, msg):
     sec = (min*60)
     while (sec) > 0:
         os.system('clear')
         print(msg)
         dt = datetime.datetime.now()
-        print("Current Time: " + str(dt.hour) + ":" + str(dt.minute) + ":" + str(dt.second) + "\n" + str(sec))
+        print("Current Time: " + str(dt.hour) + ":" + str(dt.minute) + ":" + str(dt.second))
+        print("Pomodoro Timer: " + str(convsec(sec)))
         time.sleep(1)
         sec -= 1
 
