dccee6d58675a23c94e5ace6daea6694105b44ab
Daksh Kalley <dakshk117@gmail.com>
2020-11-30 17:14:17 -0800

    Added session status

--- pomo.py
+++ pomo.py
@@ -2,12 +2,15 @@ from timer import timer, notify
 Work = int(input("Work Time: "))
 Break = int(input("Break Time: "))
 Sess = int(input("# of Sessions: "))
+Current = 1
+Total = Sess
 
 while Sess > 0:
-    timer(Work, "~~ Work ~~")
+    timer(Work, "~~ Work ~~", Current, Total)
     notify("Pomodoro", "Break Start")
-    timer(Break, "~~ Break ~~")
+    timer(Break, "~~ Break ~~", Current, Total)
     notify("Pomodoro", "Break Over")
+    Current += 1
     Sess -= 1
 
 print("Pomodoro Sessions Complete!")
--- timer.py
+++ timer.py
@@ -10,7 +10,7 @@ def convsec(seconds):
     return ("%d:%02d" % (minutes, seconds))
 
 
-def timer(min, msg):
+def timer(min, msg, curr, sess):
     sec = (min*60)
     while (sec) > 0:
         os.system('clear')
@@ -18,8 +18,9 @@ def timer(min, msg):
         dt = datetime.datetime.now()
         print("Current Time: " + str(dt.hour) + ":" + str(dt.minute) + ":" + str(dt.second))
         print("Pomodoro Timer: " + str(convsec(sec)))
+        print("Session: " + str(curr) + "/" + str(sess))
         time.sleep(1)
-        sec -= 1
+        sec -= 60
 
 def notify(title, text):
     os.system("""
